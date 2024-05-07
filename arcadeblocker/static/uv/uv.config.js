c250e4b79772ef64c1285599af6b332399b623d1
chyves <82107846+notchyves@users.noreply.github.com>
2024-05-06 20:44:59 -0400

    Update uv.config.js

--- arcadeblocker/static/uv/uv.config.js
+++ arcadeblocker/static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: '/static/tiw/',
+    prefix: 'https://arcadehubgaming.github.io/v3/static/tiw/',
     bare:'https://backend.infrared.bomberfish.ca/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/static/uv/uv.handler.js',
-    bundle: '/static/uv/uv.bundle.js',
-    config: '/static/uv/uv.config.js',
-    sw: '/static/uv/uv.sw.js',
+    handler: 'https://arcadehubgaming.github.io/v3/static/uv/uv.handler.js',
+    bundle: 'https://arcadehubgaming.github.io/v3/static/uv/uv.bundle.js',
+    config: 'https://arcadehubgaming.github.io/v3/static/uv/uv.config.js',
+    sw: 'https://arcadehubgaming.github.io/v3/static/uv/uv.sw.js',
 };
