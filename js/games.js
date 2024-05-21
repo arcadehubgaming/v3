d866f26e0e2b6c657c67e20f415e6bc18c49c60e
chyves <82107846+notchyves@users.noreply.github.com>
2024-05-21 07:22:24 -0400

    fix

--- js/games.js
+++ js/games.js
@@ -6,7 +6,7 @@ window.games = {
         "type": ["arcadehub exclusive"]
     },
     "arcadehub-uv-pr": {
-        "url": "https://iforgor.modernpotterystudio.com/",
+        "url": "https://boujour.croqueau.ch/",
         "description": "ArcadeHub's Unblocker",
         "image": "",
         "type": ["arcadehub exclusive"]
